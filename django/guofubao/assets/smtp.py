ef1048551a73a24f33a6233ee9d53b75cfa6b2f2
shs <894197954@qq.com>
2017-07-19 11:08:12 -0400

    modify the smtp.py

--- django/guofubao/assets/smtp.py
+++ django/guofubao/assets/smtp.py
@@ -12,20 +12,22 @@ import smtplib
 def _format_addr(s):
 	name,addr = parseaddr(s)
 	return formataddr((Header(name,'utf-8').encode(),addr))
-def sendmail():
+def sendmail(send):
     from_addr = "111@163.com"
     password = "aaa"
     to_addr = "222@163.com"
     smtp_server = "smtp.163.com"
     
+    print "***********************"
+    print ("send",send)
     # 邮件对象:
     msg = MIMEMultipart('alternative')
-    msg['From'] = _format_addr('快讯 <%s>' % from_addr)
-    msg['To'] = _format_addr('老王 <%s>' % to_addr)
-    msg['Subject'] = Header('快讯', 'utf-8').encode()
-    msg.attach(MIMEText('举行合作交流座谈会', 'plain', 'utf-8'))
-    #msg.attach(MIMEText('<html><body><h1>Hello</h1></body></html>', 'html', 'utf-8'))
-    
+    msg['From'] = _format_addr('运维中心 <%s>' % from_addr)
+    msg['To'] = _format_addr(' <%s>' % to_addr)
+    msg['Subject'] = Header('运维中心工单审核通知', 'utf-8').encode()
+    #msg.attach(MIMEText('中国电信海南分公司与海航通信在数据谷举行合作交流座谈会', 'plain', 'utf-8'))
+    msg.attach(MIMEText(send, 'plain', 'utf-8'))
+    msg.attach(MIMEText('<html lang="en"><head><meta charset="UTF-8"><title>Title</title></head><body><div>各位领导、同事：</div><div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;提交的工单已经通过审核，请相关人员按照工单执行，详细如下。</div><div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'+send+'</div><div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;有问题请点击并登录<a href="http://192.168.1.33:8000">运维中心</a>查询。</div></body></html>', 'html', 'utf-8'))
     
     server = smtplib.SMTP(smtp_server, 25) # SMTP协议默认端口是25
     server.set_debuglevel(1)
@@ -34,4 +36,4 @@ def sendmail():
     server.quit()
 
 if __name__ == '__main__':
-    sendmail()
+    sendmail(send)
